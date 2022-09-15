d992568ec42962dd372716c40e05638654489960
David Cuartielles <d.cuartielles@arduino.cc>
2022-09-16 01:32:28 +0200

    added thumbnails

---
+++ scripts/createHtml.php
@@ -0,0 +1,45 @@
+<?php
+	// first argument is the folder to create the index file in
+	$files = glob($argv[1]."thumbnails/*.*");
+
+	// create the index.html file
+	$myfile = fopen($argv[1]."/index.html", "w");
+
+	// prepare the file's header and footer
+	$header = "<html><header><title>GIFikOmpetition 2022 - Arduino</title></header><body>";
+	$footer = "</body></html>";
+
+	// write header
+	fwrite( $myfile,  $header );
+
+	// iterate through the files and add them to the index
+     	for ($i=0; $i<count($files); $i++)
+      	{
+        	$image = $files[$i];
+        	$supported_file = array(
+                	'gif',
+                	'jpg',
+                	'jpeg',
+                	'png'
+         	);
+
+		$filename = explode(".", basename($image));
+		$pieces = explode("_", $filename[0]);
+         	$ext = strtolower(pathinfo($image, PATHINFO_EXTENSION));
+         	if (in_array($ext, $supported_file)) {
+			fwrite( $myfile, '<div class="item"><div class="key">File name:</div><div class="value">'.basename($image)."</div>\n");
+			fwrite( $myfile, '<div class="date"><div class="key">Date:</div><div class="value">'.$pieces[0]."</div>\n");
+			fwrite( $myfile, '<div class="author"><div class="key">Author:</div><div class="value">'.$pieces[1]."</div>\n");
+			fwrite( $myfile, '<div class="title"><div class="key">Title:</div><div class="value">'.$pieces[2]."</div>\n"); 
+			fwrite( $myfile, '<div class="image"><img src="'.$image.'" alt="'.$pieces[2]." by ".$pieces[1].'" />'."</div>\n");
+            	} else {
+                	continue;
+            	}
+	}
+
+	// write footer
+	fwrite( $myfile,  $footer );
+
+	// close file object
+	fclose($myfile);
+?>
---
+++ scripts/generateThumbnails.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+## call this script with ./generateThumbnails.sh ../docs/202209/images 200
+## parameters:
+## -1 folder where to look for images
+## -2 final size [width]x[height], i.e. 400x300
+my_path=$1
+all_files=$( find $my_path -type f -name '*.jpg' -o -name '*.jpeg' -o -name '*.png' -o -name '*.gif' )
+current_path="$(pwd)"
+cd $my_path;
+cd ..;
+mkdir thumbnails;
+while read line
+do
+	filename=$(basename -- "$line")
+	extension="${filename##*.}"
+	name="${filename%.*}"
+    	echo "About to convert $name of type $extension ..."
+	if [ "$extension" = "gif" ]; then
+    		gifsicle $current_path/$line --resize-fit-width $2 > ./tmp_image && cat ./tmp_image > thumbnails/$name.$extension
+	else
+		convert $current_path/$line -resize $2 thumbnails/$name-reduced.$extension
+	fi
+    	echo "* finished: $line"
+done <<< "$all_files"
+rm ./tmp_image
+echo "* Done! *"
